ca0af81f33492e83b25ab7e5f56c18a55269d9a0
Leonardo Morini <leo.morini.nn@gmail.com>
2019-06-07 21:26:40 -0300

    base 100%

---
+++ src/services/key.js
@@ -0,0 +1,21 @@
+export const publicKey = '7e9af7a8a31946fec14f66f82fbbaee8';
+export const privateKey = '1f45c606cd63da6aaa674b968d1c964649e1a609';
+export const storageKey = '@MarvizStorage:';
+
+export const prodConfig = {
+    apiKey: "AIzaSyDw2i2cuKexrIAKNG85wEU_Y-a5HTUkEKI",
+    authDomain: "marviz.firebaseapp.com",
+    databaseURL: "https://marviz.firebaseio.com",
+    projectId: "marviz",
+    storageBucket: "marviz.appspot.com",
+    messagingSenderId: "355894380417"
+};
+
+export const devConfig = {
+    apiKey: "AIzaSyDw2i2cuKexrIAKNG85wEU_Y-a5HTUkEKI",
+    authDomain: "marviz.firebaseapp.com",
+    databaseURL: "https://marviz.firebaseio.com",
+    projectId: "marviz",
+    storageBucket: "marviz.appspot.com",
+    messagingSenderId: "355894380417"
+};
